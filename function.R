61223adae0f4d66be11df5aa0761c97c0eda3417
hlamothe <hugo.lamothe@ensae.fr>
2023-06-26 09:02:29 +0000

    Function penit_to_ts done

--- function.R
+++ function.R
@@ -1,6 +1,7 @@
 library(haven)
 library(dplyr)
-
+library(utils)
+library(tidyverse)
 penit_to_ts <- function(str){
   mens_aggreg <- read_sas("~/work/mens_agreg.sas7bdat")
   if (str == "ALL") {
@@ -20,48 +21,29 @@ penit_to_ts <- function(str){
   if (str %in% etab_ouvert){
     if (filter(d, cd_etablissement == str)$nb_quartier == 1){
       TS <- mens_aggreg %>%
-        filter(mens_aggreg, cd_etablissement = str) %>%
+        filter(cd_etablissement == str) %>%
         group_by(dt_mois) %>%
         summarise(detenus = sum(detenus))
       return(ts(TS$detenus, start = c(year(min(TS$dt_mois)), month(min(TS$dt_mois))), frequency = 12))
     } else {
-      
+      l_ts <- list()
       quartiers <- unique(filter(mens_aggreg, cd_etablissement == str)$quartier_etab)
       for (qua in quartiers) {
-        
-        
-        
-      } 
+        TS <- mens_aggreg %>%
+          filter(cd_etablissement == str & quartier_etab == qua) %>%
+          group_by(dt_mois) %>%
+          summarise(detenus = sum(detenus))
+        l_ts[[qua]] <- ts(TS$detenus, start = c(year(min(TS$dt_mois)), month(min(TS$dt_mois))), frequency = 12)
+      }
+      return(l_ts)
     }
   }
   else {
     return("L'établissement n'est plus ouvert ou le format de saisie n'est pas bon.")
   }
-  
 }
-class(year(min(TS$dt_mois)))
-TS <- mens_aggreg %>%
-  filter(cd_etablissement == numT) %>%
-  group_by(dt_mois) %>%
-  summarise(detenus = sum(detenus))
-View(TS)
-plot(ts(TS$detenus, start = c(2004, 3), frequency = 12))
-filter(d, cd_etablissement == numT5)$nb_quartier == 5
+
 numF <- "00919923"
 numT <- "00908908"
 numT5 <- "00912148"
-numF %in% etab_ouvert
-numT %in% etab_ouvert
-c(numF, numT) %in% etab_ouvert
-d <- mens_aggreg %>%
-  filter(cd_etablissement %in% etab_ouvert) %>%
-  distinct(cd_etablissement, lc_etab, type_etab, quartier_etab) %>%
-  group_by(cd_etablissement) %>%
-  mutate(nb_quartier = n_distinct(quartier_etab)) 
-d[d$cd_etablissemnt == numT]
-d <- unique(d[-4][-3])
-length(unique(d$cd_etablissement))
-last_month <- max(mens_aggreg$dt_mois)
-etab_ouvert <- unique(filter(mens_aggreg, dt_mois == last_month)$cd_etablissement)
-mens_aggreg <-rev(mens_aggreg)
 
